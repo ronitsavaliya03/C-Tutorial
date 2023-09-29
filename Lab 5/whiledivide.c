bf0b46db125dc1c0e9df2bb4f85fb3f5b4ba541e
Ronit Savaliya <143884203+ronitsavaliya03@users.noreply.github.com>
2023-09-29 13:12:44 +0530

    whiledivide.c

--- Lab 5/whiledivide.c
+++ Lab 5/whiledivide.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 int main(){
-    int n,a=1;
-    printf("enter the num1:");
+    int n,a;
+    printf("From:");
+    scanf("%d",&a);
+    printf("To:");
     scanf("%d",&n);
     
-
     while(a<=n){
         if(a%2==0){
         printf("%d\n",a);
